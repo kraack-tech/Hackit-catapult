d061a017d9088651718139b296a2d0049162a9a0
kraack-tech <kriskraack@gmail.com>
2022-11-03 09:50:19 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,22 +34,53 @@ function draw() {
   drawBalls();
   drawCatapult();
   drawGround();
+
+  //draws constraint to verify position
+/*   push();
+  stroke(255,100,100);
+  strokeWeight(2);
+  drawConstraint(constraint);
+  pop(); */
 }
 /////////////////////////////////////////////////////////////
 function setupCatapult(){
   // your code here
+  catapult = Bodies.rectangle(width/2, height-90, 600, 20);  
+  catapultSpacer = Bodies.rectangle(width/5, height, 20, 160, {isStatic: true});
+  constraint = Constraint.create({
+    pointA: { x: 400, y: height-110 },
+    bodyB: catapult,
+    pointB: { x: -10, y: -20 },
+    stiffness: 1,
+    length: 0
+  });
+  World.add(engine.world, [catapult,constraint, catapultSpacer]);
+  
 }
 /////////////////////////////////////////////////////////////
 function drawCatapult(){
   // your code here
+  noStroke();
+  fill(255);
+  drawVertices(catapult.vertices);
+  fill(255,0,0);
+  drawVertices(catapultSpacer.vertices);
 }
 /////////////////////////////////////////////////////////////
 function setupBalls(){
   // your code here
+  ball1 = Bodies.circle(600, 0, 50, {restitution:.8, density: 0.015});
+  ball2 = Bodies.circle(120, 440, 20, {restitution:.15, density: 0.001});  //restitution :15% allows it to at some point drift off the catapult
+  World.add(engine.world, [ball1, ball2]);
+
 }
 /////////////////////////////////////////////////////////////
 function drawBalls(){
   // your code here
+  noStroke();
+  fill(0, 153, 255);
+  drawVertices(ball1.vertices);
+  drawVertices(ball2.vertices);
 }
 /////////////////////////////////////////////////////////////
 function setupGround(){
@@ -72,4 +103,24 @@ function drawVertices(vertices) {
     vertex(vertices[i].x, vertices[i].y);
   }
   endShape(CLOSE);
+}
+
+
+function drawConstraint(constraint) {
+  var offsetA = constraint.pointA;
+  var posA = {x:0, y:0};
+  if (constraint.bodyA) {
+    posA = constraint.bodyA.position;
+  }
+  var offsetB = constraint.pointB;
+  var posB = {x:0, y:0};
+  if (constraint.bodyB) {
+    posB = constraint.bodyB.position;
+  }
+  line(
+    posA.x + offsetA.x,
+    posA.y + offsetA.y,
+    posB.x + offsetB.x,
+    posB.y + offsetB.y
+  );
 }
